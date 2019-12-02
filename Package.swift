c156ff942998760135fad3bd8b5c465650937feb
Mustache Developer <developer@mustache.dk>
2019-12-02 11:11:55 +0100

    Update Package.swift
    Update for Vapor 4

--- Package.swift
+++ Package.swift
@@ -1,20 +1,23 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 
 import PackageDescription
 
 let package = Package(
     name: "FCM",
+    platforms: [
+       .macOS(.v10_14)
+    ],
     products: [
         //Vapor client for Firebase Cloud Messaging
         .library(name: "FCM", targets: ["FCM"]),
     ],
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/jwt.git", from: "3.0.0"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0-beta.1"),
+        .package(url: "https://github.com/vapor/jwt-kit.git", from: "4.0.0-alpha.1.3"),
     ],
     targets: [
-        .target(name: "FCM", dependencies: ["Vapor", "JWT"]),
-        .testTarget(name: "FCMTests", dependencies: ["FCM"]),
+        .target(name: "FCM", dependencies: ["Vapor", "JWTKit"]),
+        //.testTarget(name: "FCMTests", dependencies: ["FCM"]),
     ]
 )
