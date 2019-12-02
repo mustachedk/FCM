11f549f896274919ed2ac1462bc4a315fc8c0f2e
Mustache Developer <developer@mustache.dk>
2019-12-02 11:13:49 +0100

    Update FCM+AccessToken.swift
    Update for Vapor 4

--- Sources/FCM/Helpers/FCM+AccessToken.swift
+++ Sources/FCM/Helpers/FCM+AccessToken.swift
@@ -2,30 +2,38 @@ import Foundation
 import Vapor
 
 extension FCM {
-    func getAccessToken(_ client: Client) throws -> Future<String> {
-        if !gAuthPayload.hasExpired, let token = accessToken {
-            return client.container.eventLoop.newSucceededFuture(result: token)
-        }
+    func getAccessToken(_ client: Client) throws -> EventLoopFuture<String> {
+        if !gAuthPayload.hasExpired, let token = accessToken { return client.eventLoopGroup.future(token) }
+
         var payload: [String: String] = [:]
         payload["grant_type"] = "urn:ietf:params:oauth:grant-type:jwt-bearer"
-        payload["assertion"] = try getJWT()
-        return client.post(audience) { req throws in
-            try req.content.encode(payload, as: .urlEncodedForm)
-        }.map { res in
-            struct Result: Codable {
-                var access_token: String
-            }
-            guard let data = res.http.body.data else {
-                throw Abort(.notFound, reason: "Data not found")
-            }
-            if res.http.status.code != 200 {
-                let code = "Code: \(res.http.status.code)"
-                let message = "Message: \(String(data: data, encoding: .utf8) ?? "n/a")"
-                let reason = "[FCM] Unable to refresh access token. \(code) \(message)"
-                throw Abort(.internalServerError, reason: reason)
-            }
-            let result = try JSONDecoder().decode(Result.self, from: data)
-            return result.access_token
-        }
+        payload["assertion"] = try self.getJWT()
+
+        let uri = URI(path: self.audience)
+        return client.post(uri) { (request: inout ClientRequest) throws in
+                    try request.content.encode(payload, as: .urlEncodedForm)
+                }
+                .flatMapThrowing( { (response: ClientResponse) in
+                    guard var body = response.body else {
+                        throw Abort(.notFound, reason: "Data not found")
+                    }
+                    if response.status != .ok {
+                        let code = "Code: \(response.status)"
+                        let string = body.readString(length: body.readableBytes)
+                        let message = "Message: \(string ?? "n/a")"
+                        let reason = "[FCM] Unable to refresh access token. \(code) \(message)"
+                        throw Abort(.internalServerError, reason: reason)
+                    }
+
+                    struct Result: Codable {
+                        var access_token: String
+                    }
+                    
+                    let result = try response.content.decode(Result.self)
+                    return result.access_token
+                })
     }
+
+
+
 }
