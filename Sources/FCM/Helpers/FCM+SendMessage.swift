66869cff395ea08622fef7e7cd05dbdb1cf65738
Mustache Developer <developer@mustache.dk>
2019-12-02 11:14:40 +0100

    Update FCM+SendMessage.swift
    Update for Vapor 4

--- Sources/FCM/Helpers/FCM+SendMessage.swift
+++ Sources/FCM/Helpers/FCM+SendMessage.swift
@@ -2,7 +2,8 @@ import Foundation
 import Vapor
 
 extension FCM {
-    public func sendMessage(_ client: Client, message: FCMMessageDefault) throws -> Future<String> {
+
+    public func sendMessage(_ client: Client, message: FCMMessageDefault) throws -> EventLoopFuture<String> {
         if message.apns == nil,
             let apnsDefaultConfig = apnsDefaultConfig {
             message.apns = apnsDefaultConfig
@@ -20,32 +21,41 @@ extension FCM {
             var headers = HTTPHeaders()
             headers.add(name: "Authorization", value: "Bearer "+accessToken)
             headers.add(name: "Content-Type", value: "application/json")
-            return client.post(url, headers: headers) { req throws in
+            return client.post(URI(string: url), headers: headers) { (request: inout ClientRequest) throws in
+
                 struct Payload: Codable {
-                    var validate_only: Bool
-                    var message: FCMMessageDefault
-                }
+                       var validate_only: Bool
+                       var message: FCMMessageDefault
+                   }
+
                 let payload = Payload(validate_only: false, message: message)
-                try req.content.encode(payload, as: .json)
-            }.map { res in
-                guard let data = res.http.body.data else {
+                try request.content.encode(payload, as: .json)
+            }
+            .flatMapThrowing( { (response: ClientResponse) in
+                guard var body = response.body else {
                     throw Abort(.notFound, reason: "Data not found")
                 }
 
-                guard 200 ..< 300 ~= res.http.status.code else {
-                    if let googleError = try? res.content.syncDecode(GoogleError.self) {
+                guard 200 ..< 300 ~= response.status.code else {
+                    if let googleError = try? response.content.decode(GoogleError.self) {
                         throw googleError
                     } else {
-                        let reason = String(data: data, encoding: .utf8) ?? "Unable to decode Firebase response"
+                        let string = body.readString(length: body.readableBytes)
+                        let reason = string ?? "Unable to decode Firebase response"
                         throw Abort(.internalServerError, reason: reason)
                     }
                 }
-
                 struct Result: Codable {
                     var name: String
                 }
-                return try res.content.syncDecode(Result.self).name
-            }
+                
+                return try response.content.decode(Result.self).name
+            })
         }
     }
+
+
+
+
+
 }
